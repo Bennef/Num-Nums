112a50fdf9a56a3917141401e6836ddb3ed730e6
Ben <benneftobennef@hotmail.com>
2020-07-07 18:12:23 +0100

    Camera shake fixed.

---
+++ Assets/_Project/Scripts/Obstacles/ObstacleMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    [SerializeField] int _rotationSpeed;
+
+    void Update()
+    {
+        if (_rotationSpeed != 0)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * _rotationSpeed);
+        }        
+    }
+}
